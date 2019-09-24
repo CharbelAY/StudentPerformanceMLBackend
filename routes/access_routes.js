ee1b80581c9e407725de68ffe1e8fe8dd619650c
Charbelay <charbel@abouyounes.com>
2019-09-25 02:42:27 +0300

    REST api done still having issues with the update route

--- routes/access_routes.js
+++ routes/access_routes.js
@@ -19,8 +19,8 @@ router.get('/', async (req,res) =>{
 
 //getting one
 
-router.get('/:id',(req,res) =>{
-    res.send(req.params.id)
+router.get('/:id', getAnswers,(req,res) =>{
+    res.send(res.answers)
 })
 
 //creating one
@@ -41,12 +41,45 @@ router.post('/',async (req,res) =>{
 
 //updating one
 
-router.patch('/',(req,res) =>{
-    
+router.patch('/:id',getAnswers,async (req,res) =>{
+    if(req.body.question != null){
+        res.answers.question = req.body.question
+    }
+
+    if(req.body.answers != null){
+        res.answers.answers = req.body.answers
+    }
+
+    try{
+        const updatedAnswers = await res.answers.save()
+        res.json(updatedAnswers)
+    }catch(err){
+        res.status(400).json({message:err.message})
+    }
 })
 
 //deleting one
 
-router.delete('/:id',(req,res) =>{
-    
+router.delete('/:id',getAnswers,async (req,res) =>{
+    try{
+        await res.answers.remove()
+        res.json({"message":"Subscriber deleted"})
+    }catch(err){
+        res.status(500).json({message:err.message})
+    }
 })
+
+async function getAnswers(req,res,next){
+    let answers
+    try{
+        answers = await Answers.findById(req.params.id)
+        if(answers == null){
+            return res.status(404).json("Cannot find answers")
+        }
+    }catch(err){
+        return res.status(500).json({message:err.message})
+    }
+
+    res.answers = answers
+    next()
+}
